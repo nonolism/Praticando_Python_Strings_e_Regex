91b7b0d1f75679783a0a349cfda1c218c9e1d12b
Nonolism <ruanvitormoreno@outlook.com>
2025-06-29 18:32:29 -0300

    Decifrando pistas com palavras-chave

---
+++ atividade_03/main.py
@@ -0,0 +1,6 @@
+def main():
+    palavra_chave = input("Digite a palavra-chave: ")
+    print(f"Primeiras : {palavra_chave[:3]} \nUltimas : {palavra_chave[-3:]}")
+
+if __name__ == "__main__":
+    main()
